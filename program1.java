eda9c48eb78393eb33aacf750a9dab0aa3e1a19b
pallamahalakshmi <127485928+pallamahalakshmi@users.noreply.github.com>
2023-03-10 15:51:52 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,7 @@
+package com.testingautomation;
+
+public class program1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to testing automation classes");
+    }
+}
